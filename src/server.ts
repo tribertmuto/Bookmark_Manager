a05d3cab5442b03923c5943d2d48b349cf847d6e
tribertmuto <tribertmuto@gmail.com>
2025-09-20 19:11:28 +0000

    chore: First commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app from "./app";
+import { ENV } from "./config/env";
+import { connectDB } from "./config/db";
+
+connectDB();
+
+app.listen(ENV.PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${ENV.PORT}`);
+});
